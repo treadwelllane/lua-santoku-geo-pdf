a5881d166d4eec3b727755e4445d96cf079eeff0
Matthew Brooks <mb964033@gmail.com>
2023-12-12 21:25:09 +0000

    update santoku versions

--- config.lua
+++ config.lua
@@ -1,20 +1,20 @@
 local env = {
 
   name = "santoku-geo-pdf",
-  version = "0.0.2-1",
+  version = "0.0.3-1",
   variable_prefix = "TK_GEO_PDF",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
+    "santoku >= 0.0.145-1",
     "lua-zlib >= 1.2-2",
   },
 
   test_dependencies = {
+    "santoku-test >= 0.0.2-1",
     "luassert >= 1.9.0-1",
-    "luaposix >= 36.2.1-1",
-    "santoku >= 0.0.142-1",
     "luacheck >= 1.1.0-1",
     "luacov >= 0.15.0-1",
   },
